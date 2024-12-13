2e54c6ea77891ca9608310192eca1a9e5804c93e
1F10240059 <s1F102400594@iniad.org>
2024-12-13 15:09:30 +0900

    add

--- teamapp/urls.py
+++ teamapp/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_screen, name='home_screen')
+    path('', views.home_screen, name=home_screen),  
 ]
--- teamapp/views.py
+++ teamapp/views.py
@@ -1,5 +1,67 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
 
 # Create your views here.
 def home_screen(request):
     return render(request, "teamapp/home_screen.html")
+
+def index(request):
+    if request.method == 'POST':
+        article = Article(title=request.POST['title'], body=request.POST['text'])
+        article.save()
+        return redirect(detail, article.id)
+
+    if ('sort' in request.GET):
+        if request.GET['sort'] == 'like':
+            articles = Article.objects.order_by('-like')
+        else:
+            articles = Article.objects.order_by('-posted_at')
+    else:
+        articles = Article.objects.order_by('-posted_at')
+
+    context = {
+        "articles": articles,
+    }
+    return render(request, 'blog/index.html', context)
+
+def detail(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+
+    if request.method == 'POST':
+        comment = Comment(article=article, text=request.POST['text'])
+        comment.save()
+    context = {
+        'article': article,
+        'comments': article.comments.order_by('-posted_at')
+    }
+    return render(request, 'blog/detail.html', context)
+
+def update(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Http404:
+        raise Http404("Article does not exist")
+    if request.method == 'POST':
+        article.title = request.POST['title']
+        article.body = request.POST['text']
+        article.save()
+        return redirect(detail, article_id)
+    context = {
+        'article': article
+    }
+    return render(request, 'blog/edit.html', context)
+
+def delete(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404('Article does not exist')
+    article.delete()
+    return redirect(index)
