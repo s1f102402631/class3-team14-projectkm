c0bb845d62cbf1cf44cca306a91adb43c7a8619a
s1f102401922 <s1f102401922@iniad.org>
2024-12-27 13:05:03 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = ['teamapp.backends.CustomAuthBackend']
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/bio/'
--- teamapp/models.py
+++ teamapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here
 
@@ -32,3 +34,14 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return self.username
+    
+class Profile(models.Model):
+    #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #studentid = models.CharField(max_length=10)
+    self_info = models.TextField()
+    mbti = models.CharField(max_length=4)
+    hobby = models.CharField(max_length=255)
+    fav = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.self_info
