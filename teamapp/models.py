6f707249160b8c6f322eaf7628e9430d5e1196d3
1F10240126 <s1F102401269@iniad.org>
2025-01-08 15:54:00 +0900

    Update models.py

--- teamapp/models.py
+++ teamapp/models.py
@@ -59,3 +59,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
